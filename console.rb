e469336e35c106b04b6481f9cc9c1b02da593721
Craig Bowditch <craigbowditch@hotmail.com>
2018-08-04 12:57:42 +0100

    Creates CRUD for ticket class and passes tests

--- console.rb
+++ console.rb
@@ -1,9 +1,10 @@
 require_relative('models/customer.rb')
 require_relative('models/film.rb')
+require_relative('models/ticket.rb')
 require_relative('db/sql_runner')
 require ('pry')
 
-
+Ticket.delete_all()
 Customer.delete_all()
 Film.delete_all()
 
@@ -20,6 +21,13 @@ film1 = Film.new(
     'price' => 10
     })
     film1.save()
+
+ticket1 = Ticket.new(
+  {
+    'customer_id' => customer1.id,
+    'film_id' => film1.id
+    })
+    ticket1.save()
 
     binding.pry
     nil
---
+++ models/ticket.rb
@@ -0,0 +1,43 @@
+require_relative('../db/sql_runner')
+
+class Ticket
+
+  attr_reader :id, :customer_id, :film_id
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @customer_id = options['customer_id'].to_i()
+    @film_id = options['film_id'].to_i()
+  end
+
+  def save()
+    sql = "INSERT INTO tickets
+    (
+    customer_id, film_id
+     )
+    VALUES ($1, $2)
+    RETURNING id"
+    values = [@customer_id, @film_id]
+    results = SqlRunner.run(sql, values).first
+    @id = results['id'].to_i()
+  end
+
+  def self.all()
+    sql = "SELECT * FROM tickets"
+    tickets = SqlRunner.run(sql)
+    results = tickets.map { |ticket| Ticket.new(ticket)  }
+    return results
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM tickets"
+    results = SqlRunner.run(sql)
+  end
+
+  def delete_one()
+    sql = "DELETE FROM tickets WHERE id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql,values)
+  end
+
+end
