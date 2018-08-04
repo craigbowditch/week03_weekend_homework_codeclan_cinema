a00ab42652788b8d584e87bd8df29edfaf8dc5cf
Craig Bowditch <craigbowditch@hotmail.com>
2018-08-04 12:49:12 +0100

    Creates CRUD for film class and passes tests

--- console.rb
+++ console.rb
@@ -1,9 +1,11 @@
 require_relative('models/customer.rb')
+require_relative('models/film.rb')
 require_relative('db/sql_runner')
 require ('pry')
 
 
 Customer.delete_all()
+Film.delete_all()
 
 customer1 = Customer.new(
   {
@@ -12,6 +14,12 @@ customer1 = Customer.new(
     })
     customer1.save()
 
+film1 = Film.new(
+  {
+    'title' => "The Royal Tenenbaums",
+    'price' => 10
+    })
+    film1.save()
 
     binding.pry
     nil
