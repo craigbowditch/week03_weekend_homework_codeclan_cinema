53fb48234569474db67401f90deccc8610abac7c
Craig Bowditch <craigbowditch@hotmail.com>
2018-08-04 13:10:12 +0100

    Completes MVP

--- models/customer.rb
+++ models/customer.rb
@@ -44,4 +44,13 @@ attr_accessor :name, :funds
     results = SqlRunner.run(sql, values)
   end
 
+  def films()
+    sql = "SELECT films.* FROM films
+    INNER JOIN tickets ON films.id = tickets.film_id
+    WHERE tickets.customer_id = $1"
+    values = [@id]
+    films = SqlRunner.run(sql, values)
+    return films.map { |film| Film.new(film)  }
+  end
+
 end
--- models/film.rb
+++ models/film.rb
@@ -44,4 +44,13 @@ attr_accessor :title, :price
     results = SqlRunner.run(sql, values)
   end
 
+  def customers()
+    sql = "SELECT customers.* FROM customers
+    INNER JOIN tickets ON customers.id = tickets.customer_id
+    WHERE tickets.film_id = $1"
+    values = [@id]
+    customers = SqlRunner.run(sql, values)
+    return customers.map { |customer| Customer.new(customer)  }
+  end
+
 end
